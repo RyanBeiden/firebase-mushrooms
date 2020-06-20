9ec45edbae2eaec3f617395275a0c74ea0bbd30b
Ryan Beiden <rdbeiden@gmail.com>
2020-06-20 10:47:15 -0500

    js prints login button to DOM & google api added

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,18 @@
+import auth from './components/auth/auth';
+
 import 'bootstrap';
 import '../styles/main.scss';
 
-console.warn('JS Works like a charm');
+/**
+ * Be able to 'log in' to our app
+ * Be able to 'log out' of our app
+ * See a login button if we are not logged in
+ * See a logout if we ARE logged in
+ * See a list of mushrooms (like a forest!) if we ARE logged in
+ */
+
+const init = () => {
+  auth.loginButton();
+};
+
+init();
