a221a2c03537451ec3da1dee5dbaf244aab0b837
Ryan Beiden <rdbeiden@gmail.com>
2020-06-20 11:57:29 -0500

    add login/logout button with full functionality

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,11 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import utils from '../../helpers/utils';
 
 const signMeIn = () => {
-  console.warn('Yea, I\'ll sign you in, just hold up one sec');
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(googleProvider);
 };
 
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-warning"><i class="fab fa-google"></i> LOG ME IN</button>';
+  const domString = `
+  <div class="d-flex justify-content-center mt-5">
+    <button id="google-auth" class="btn btn-warning"><i class="fab fa-google"></i> - Login</button>
+  </div>
+  `;
   utils.printToDom('#auth', domString);
   $('#google-auth').click(signMeIn);
 };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,10 @@
-import auth from './components/auth/auth';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 
-import 'bootstrap';
+import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
+import authData from './helpers/data/authData';
+
 import '../styles/main.scss';
 
 /**
@@ -12,7 +16,10 @@ import '../styles/main.scss';
  */
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
