ff68ae7d644ed44abfe21965334c37a7f7fc6bb2
Ryan Beiden <rdbeiden@gmail.com>
2020-06-22 19:30:03 -0500

    create mushroom compnent and use it to display all shrooms

--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -1,12 +1,25 @@
 import mushroomData from '../../helpers/data/mushroomData';
-// import utils from '../../helpers/utils';
+import utils from '../../helpers/utils';
+import mushroomComponent from '../mushroom/mushroom';
 
 const buildForest = () => {
   mushroomData.getMushrooms()
-    .then((mushrooms) => console.warn('Get mushrooms works!!', mushrooms))
+    .then((mushrooms) => {
+      console.warn('Get mushrooms works!!', mushrooms);
+      let domString = `
+        <h2 class="text-center">Forest</h2>
+        <div class="d-flex flex-wrap">
+      `;
+
+      mushrooms.forEach((mushroom) => {
+        domString += mushroomComponent.mushroomMaker(mushroom);
+      });
+
+      domString += '</div>';
+
+      utils.printToDom('#forest', domString);
+    })
     .catch((err) => console.error('get mushrooms broke...', err));
-  // const domString = 'I SEE!!!';
-  // utils.printToDom('#forest', domString);
 };
 
 export default { buildForest };
