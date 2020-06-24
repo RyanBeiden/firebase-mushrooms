54ac0707afaba8fe29d504c74ba13ee78a325254
Ryan Beiden <rdbeiden@gmail.com>
2020-06-23 20:17:45 -0500

    print mycologist's mushrooms to dom

--- src/javascripts/components/singleMycologist/singleMycologist.js
+++ src/javascripts/components/singleMycologist/singleMycologist.js
@@ -5,11 +5,18 @@ const buildMycologist = (e) => {
   const mycologistId = e.target.closest('.card').id;
   smash.getSingleMycoWithShrooms(mycologistId)
     .then((mycologist) => {
-      console.warn('here is your mycologist:', mycologist);
-      const domString = `
-      <div style="background-color: yellow; padding: 15px; margin: 45px 15px;">
-        <h1>Featured Mycologist</h1>
+      let domString = `
+      <div style="background-color: #FFC60A; padding: 15px; margin: 45px 15px;">
+        <h1 style="color: #fff;">Featured Mycologist</h1>
         <h3>${mycologist.name} (Age: ${mycologist.age})</h3>
+      <div style="border: 2px solid #fff; padding: 15px;" class="mt-3">
+        <h5>Mushroom(s) Owned:</h5>
+      `;
+      mycologist.mushrooms.forEach((mushroom) => {
+        domString += `<p>${mushroom.name}</p>`;
+      });
+      domString += `
+        </div>
       </div>
       `;
       utils.printToDom('#single-myco', domString);
