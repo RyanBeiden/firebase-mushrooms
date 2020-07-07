16db0fdb5290ebb1c46334470dc54db806f0e313
Ryan Beiden <rdbeiden@gmail.com>
2020-07-06 21:34:38 -0500

    add form & functionality for adding a new mycologist I am running into an issue here though when the first myco or shroom is added, it prints correctly. But, the second time it prints whatever you typed into the form twice, then the tird time it prints 3 times and so on. I am not sure what the issue here is...

--- src/javascripts/components/mycologistList/mycologistList.js
+++ src/javascripts/components/mycologistList/mycologistList.js
@@ -1,13 +1,38 @@
+import Prando from 'prando';
 import mycologistData from '../../helpers/data/mycologistData';
 import utils from '../../helpers/utils';
 import mycologistComponent from '../mycologist/mycologist';
 import singleMycologist from '../singleMycologist/singleMycologist';
+import newMycologist from '../newMycologist/newMycologist';
+import './mycologistList.scss';
+
+const addMycoFormData = (e) => {
+  e.preventDefault();
+  const rng = new Prando();
+
+  const newMyco = {
+    name: $('#myco-name').val(),
+    age: $('#myco-age').val() * 1,
+    uid: rng.nextInt(0, 1000000000000000000000),
+  };
+
+  mycologistData.addMyco(newMyco)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      buildHut();
+      utils.printToDom('#new-myco', '');
+    })
+    .catch((err) => console.error('could not add mycologist', err));
+};
 
 const buildHut = () => {
   mycologistData.getMycologists()
     .then((mycologists) => {
       let domString = `
         <h2 class="text-center mb-2 mt-5">Mycologist Hut</h2>
+          <div class="d-flex justify-content-center mb-4 mt-4">
+            <button class="btn btn-warning" id="show-myco-form">New Mycologist <i class="fas fa-plus-circle"></i></button>
+          </div>
         <div class="d-flex justify-content-center flex-wrap">
       `;
       mycologists.forEach((mycologist) => {
@@ -17,6 +42,8 @@ const buildHut = () => {
       utils.printToDom('#hut', domString);
 
       $('body').on('click', '.myco-card', singleMycologist.buildMycologist);
+      $('body').on('click', '#show-myco-form', newMycologist.showMycoForm);
+      $('body').on('click', '#myco-creator', addMycoFormData);
     })
     .catch((err) => console.error('something broke here:', err));
 };
