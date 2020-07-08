2bb771c2ce54aab918b314923b9ff1b1e590ab9c
Ryan Beiden <rdbeiden@gmail.com>
2020-07-07 19:28:30 -0500

    add smash function for isChecked object key

--- src/javascripts/helpers/data/mycologistMushroomData.js
+++ src/javascripts/helpers/data/mycologistMushroomData.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 import apiKeys from '../apiKeys.json';
+import utils from '../utils';
 
 const baseUrl = apiKeys.firebaseConfig.databaseURL;
+
+const getAllMycoShrooms = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/mycologistMushrooms.json`)
+    .then(({ data }) => resolve(utils.convertFirebaseCollection(data)))
+    .catch((err) => reject(err));
+});
 
 const getMycoShroomsByMycoUid = (mycoUid) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/mycologistMushrooms.json?orderBy="mycologistUid"&equalTo="${mycoUid}"`)
